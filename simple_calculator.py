7bcb29b9ba1cdc6d11ec0b7dcf3c4a8b662bb99a
Shubham Patel <131590121+ShubhamPatel81@users.noreply.github.com>
2023-07-15 13:46:45 +0530

    Update simple_calculator.py "number not divisible by zero"

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,46 +1,44 @@
-# addition
-
-
+# Simple Basic Calculator 
 def add(p, q):
     return p + q
 
 
 # subtraction
 
-
 def subtraction(p, q):
     return p - q
 
-
 # Multiplication
-
 
 def multiplication(p, q):
     return p * q
 
-
 # Division
 
-
-def division(p, q):
-    return p / q
-
-
-print("Enter your choice ")
-print("a : for addition ")
-print("s : for substraction ")
-print("m : for multiplication ")
-print("d :for division")
-num_1 = int(input("Enter first number "))
-choice = input("Enter your choice ")
-num_2 = int(input("Enter second number "))
-if choice == 'a':
-    print(num_1, " + ", num_2, " = ", add(num_1, num_2))
-elif choice == 's':
-    print(num_1, " - ", num_2, " = ", subtraction(num_1, num_2))
-elif choice == 'm':
-    print(num_1, " * ", num_2, " = ", multiplication(num_1, num_2))
-elif choice == 'd':
-    print(num_1, " / ", num_2, " = ", division(num_1, num_2))
-else:
-    print("Invalid choice ")
+try:
+    def division(p, q):
+        return p / q
+except ZeroDivisionError as e:
+    print(e)
+while (True):
+    print("Enter your choice ")
+    print("a : For addition ")
+    print("s : For substraction ")
+    print("m : For multiplication ")
+    print("d :For division")
+    print("Enter zero for exit : ")
+    choice = input("Enter your choice ")
+    num_1 = int(input("Enter first number "))
+    num_2 = int(input("Enter second number "))
+    if choice == 'a':
+        print(num_1, " + ", num_2, " = ", add(num_1, num_2))
+    elif choice == 's':
+        print(num_1, " - ", num_2, " = ", subtraction(num_1, num_2))
+    elif choice == 'm':
+        print(num_1, " * ", num_2, " = ", multiplication(num_1, num_2))
+    elif choice == 'd':
+        print(num_1, " / ", num_2, " = ", division(num_1, num_2))
+    elif choice == "0":
+        exit()
+    else:
+        print("Invalid choice ")
